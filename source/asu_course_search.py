d11efc609872c3753a710d01c1d5a7889992f744
Damion <damionotg@gmail.com>
2019-06-27 16:03:05 -0700

    Separated control classes from app class
    Moved all control classes into their own file: controls.py. Defined basic structure of application class ASUCourseSearch.

--- source/asu_course_search.py
+++ source/asu_course_search.py
@@ -1,140 +1,52 @@
-from abc import ABC, abstractmethod
-from typing import Set, Union, Optional, Dict, Tuple
+from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
-from selenium.webdriver.remote.webelement import WebElement
-from selenium.webdriver.common.by import By
-from source.util import move_and_click
-from source.util import wait_load
+from typing import Optional, Union, Set, Dict
+from source.controls import AbstractControl, InPersonOrOnline, OpenOrAll, \
+                             Term, Subject, Number, Keyword, Session, Location
 
 
-# Models the base required methods of a control class
-#   typing here is used for documentation purposes,
-#   it is not enforceable with Python's design
-class AbstractControl(ABC):
+# Class to represent the entire asu-course-search program
+class ASUCourseSearch:
 
-    # Input: reference(s) to shared application resources
-    # Output: -
-    @abstractmethod
-    def __init__(self, driver: WebDriver):
+    def __init__(self):
 
-        # Reference to the Selenium web driver instance
-        self.driver = driver
+        # Create driver, set timeout, fetch course search page
+        self.driver: WebDriver = \
+            webdriver.Chrome('C:/ChromeDriver/chromedriver.exe')
+        self.driver.implicitly_wait(10)
+        self.driver.get('https://webapp4.asu.edu/catalog/classlist')
 
-        # Holds the set of valid option strings
-        self.valid_options: Optional[Set[str]] = None
-
-    # Input: -
-    # Output: set of valid option strings
-    @abstractmethod
-    def checker(self) -> Set[str]:
-        pass
-
-    # Input: string or set of strings that are the choice
-    #   -str: for controls that can only hold 1 value
-    #   -Set[str]: for controls that allow multi-selection
-    # Output: -
-    @abstractmethod
-    def setter(self, value: Union[str, Set[str]]) -> None:
-        pass
-
-    # Input: -
-    # Output: string that is the current value on the page
-    @abstractmethod
-    def getter(self) -> Optional[Union[str, Set[str]]]:
-        pass
-
-
-# Models a radio-group control element defined by the options argument
-class RadioGroup(AbstractControl):
-
-    # options is used to add radios to the set upon creation. Example Below:
-    # options = {
-    #   'open': (By.ID, 'searchTypeOpen'),
-    #   'all': (By.ID, 'searchTypeAllClass')
-    # }
-    def __init__(self, driver: WebDriver, options: Dict[str, Tuple[str, str]]):
-        super().__init__(driver)
-
-        # Pull valid option set from options dict
-        self.valid_options = set(options.keys())
-
-        # Create mapper for valid option to according radio element
-        self.option_to_radio: Dict[str, WebElement] = {}
-        for option in self.valid_options:
-            radio_element = self.driver.find_element(*options[option])
-            self.option_to_radio[option] = radio_element
-
-    # Return valid options for radio set
-    def checker(self) -> Set[str]:
-        return self.valid_options
-
-    # Set option to value
-    def setter(self, value: str) -> None:
-
-        # If value is invalid, warn and state valid options
-        if value not in self.valid_options:
-            print('Could not pick radio named [' + value + '].')
-            print('Valid options are: ' + str(self.valid_options))
-            return
-
-        # If value is valid and selection must change, change it
-        if self.getter() != value:
-            radio_element = self.option_to_radio[value]
-            move_and_click(self.driver, radio_element)
-            wait_load(self.driver)
-
-    # Return name of selected radio, or None if neither are selected
-    def getter(self) -> Optional[str]:
-
-        # Go through radios and return the name of the checked radio
-        for option in self.valid_options:
-            if self.option_to_radio[option].get_attribute('checked'):
-                return option
-
-        # If no radio was checked, return None
-        return None
-
-
-class InPersonOrOnline(RadioGroup):
-
-    def __init__(self, driver: WebDriver):
-
-        options = {
-            'person': (By.ID, 'radio-campus'),
-            'online': (By.ID, 'radio-online')
+        # Maps name of control to its instance
+        self.controls: Dict[str, AbstractControl] = {
+            'PersonOnline': InPersonOrOnline(self.driver),
+            'Term': Term(self.driver),
+            'Subject': Subject(self.driver),
+            'Number': Number(self.driver),
+            'Keyword': Keyword(self.driver),
+            'Session': Session(self.driver),
+            'Location': Location(self.driver),
+            'OpenAll': OpenOrAll(self.driver)
         }
-        super().__init__(driver, options)
 
+    # Quit program and close selenium instance
+    def quit(self):
+        self.driver.quit()
 
-class OpenOrAll(RadioGroup):
+    # Returns the valid option names of all AbstractControls
+    def checker(self) -> Set[str]:
+        return set(self.controls.keys())
 
-    def __init__(self, driver: WebDriver):
+    # Gets the valid options of the AbstractControl named control
+    def control_checker(self, control: str) -> Set[str]:
+        pass
 
-        options = {
-            'open': (By.ID, 'searchTypeOpen'),
-            'all': (By.ID, 'searchTypeAllClass')
-        }
-        super().__init__(driver, options)
+    # Sets the AbstractControl named control to value
+    def control_setter(self, control: str,
+                       value: Union[str, Set[str]]) -> None:
+        pass
 
-
-class Term(AbstractControl):
-    pass
-
-
-class Subject(AbstractControl):
-    pass
-
-class Number(AbstractControl):
-    pass
-
-
-class Keyword(AbstractControl):
-    pass
-
-
-class Session(AbstractControl):
-    pass
-
-
-class Location(AbstractControl):
-    pass
+    # Returns the current value of the AbstractControl instance named control
+    #   -Single selection controls return str or None
+    #   -Multi-section controls return Set[str] (can be empty for no-selection)
+    def control_getter(self, control: str) -> Optional[Union[str, Set[str]]]:
+        pass
